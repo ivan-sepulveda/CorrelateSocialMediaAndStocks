4c9da37695936a14be6de604443397f52d217fd1
ivan-sepulveda <ivan.sepulveda2014@gmail.com>
2018-09-17 12:50:22 -0700

    removed dead code

--- aws.py
+++
@@ -1,2 +0,0 @@
-print("Hello Webpage!")
-print("From private repo!")
--- outerLoop.py
+++ outerLoop.py
@@ -6,9 +6,9 @@ from stockAnalysis import *
 from plotSocialMediaVsStocks import *
 
 # Now we'll pick a retailer by inputting both their IG handle and
-retailerIg = "adidas"
+retailerIg = "nike"
 # Now let's enter the same retailers NASDAQ index
-retailerNasdaq = "ADDYY"
+retailerNasdaq = "NKE"
 # alpha_vantage key
 av_key = "HQXQHIFN548EZELL"
 
@@ -24,22 +24,10 @@ nasdaqData = pullStockPriceHistoryData(av_key, retailerNasdaq)
 # Step 4: Parse through data and run analytics
 stockPriceMetrics = returnPercentChangesByDate(nasdaqData)
 # Step 5: Plot both metrics against each other
-print(socialMediaMetrics.keys())
 stockSeries = convertToPandaSeries(stockPriceMetrics["price_percent_changes"], stockPriceMetrics["date_strings"])
 mediaSeries = convertToPandaSeries(socialMediaMetrics["likes_percent_changes"], socialMediaMetrics["timestamps"])
 plotSocialMediaVsStocks(mediaSeries, stockSeries)
 
-
-
-
-
-
-
-# # first lemme compare the date range
-# print("stocks")
-# print(stockPriceMetrics["date_strings"])
-# print("social media")
-# print(socialMediaMetrics["timestamps"])
 
 
 
